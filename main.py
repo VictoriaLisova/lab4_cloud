301967e233a15e58042e11733ea038f83f47661b
Victoria Lisova <lisovaviktoria@knu.ua>
2025-11-23 20:44:44 +0200

    add docker and docker-compose files

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,4 @@ def home():
     return "This is the test program that says:<br>Hello world"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
